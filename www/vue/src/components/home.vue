e1034c6f5f8d05f86f0cfb0bd23cb3c3075a2b6c
刘帅鹏 <shuaipengliu@foxmail.com>
2019-01-16 16:56:34 +0800

    home页面更改

--- www/vue/src/components/home.vue
+++ www/vue/src/components/home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
-    <router-link to="/lottery">抽奖展示页面</router-link>
-    <router-link to="/lotteryAdmin">抽奖后台页面</router-link>
+    <el-card>
+      <el-button type="primary"
+        @click="routerGo('/lottery')">
+        抽奖页面
+      </el-button>
+      <el-button type="primary"
+        @click="routerGo('/vote')">
+        投票页面
+      </el-button>
+    </el-card>
   </div>
 </template>
 
@@ -12,10 +20,18 @@ export default {
     return {
       msg: "Welcome to Your Vue.js App"
     };
+  },
+  methods: {
+    routerGo(url) {
+      this.$router.push(url);
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.home {
+  padding: 20px;
+}
 </style>
